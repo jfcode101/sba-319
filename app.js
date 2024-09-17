427b40bbce58391e6ba896230fa58dcbbda53045
jfcode101 <125421939+jfcode101@users.noreply.github.com>
2024-09-17 08:43:09 -0400

    adds a connection to the database

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+require("dotenv").config(); // load environment variable from .env
 
 // assign express to app constant variable
 const app = express();
@@ -8,7 +9,28 @@ const PORT = process.env.PORT || 5500;
 // use the express.json() middleware to parse incoming JSON request
 app.use(express.json());
 
-//
+// retrieve the database connection string from
+// environment variable
+const mongoStrURI = process.env.DATABASE_URL;
+
+// connect the database to the server
+mongoose
+  .connect(mongoStrURI)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+
+// when the database connection fails, throw an error
+
+// create a basic router
+app.get("/", (req, res) => {
+  res.send("SBA-19 loading!");
+});
+
+// listen to the port
 app.listen(PORT, () => {
   console.log(`App is listenning on port: ${PORT}`);
 });
